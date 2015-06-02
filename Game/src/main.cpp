b4ac4c59c29790f50e626c1edc61d65813f2027c
aceross <aaron.ceross@gmail.com>
2015-06-02 10:47:49 +0100

    Start of game code for MSc Thesis

---
+++ Game/src/main.cpp
@@ -0,0 +1,12 @@
+/* ----------------------------------------------------------------------------
+    Copyright 2015, Aaron Ceross
+
+    Source code submitted for the MSc Thesis, as part of the
+    completion of the MSc in Computer Science at the University of Bristol
+------------------------------------------------------------------------------*/
+
+#include <iostream>
+
+int main() {
+  std::cout << " Hello Project!";
+}
