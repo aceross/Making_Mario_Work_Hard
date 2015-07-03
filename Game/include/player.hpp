40b5b64c8e20779ad34b7d89efdfeae8b60a44c5
aceross <aaron.ceross@gmail.com>
2015-07-03 15:44:18 +0100

    Player source re-written

--- Game/include/player.hpp
+++ Game/include/player.hpp
@@ -3,39 +3,47 @@
 #ifndef PLAYER_HPP
 #define PLAYER_HPP
 
-#include <SFML/Graphics.hpp>
+#include <SFML/Graphics/RenderTarget.hpp>
+#include <SFML/System/Time.hpp>
+#include <SFML/Graphics/Drawable.hpp>
+#include <SFML/Graphics/Transformable.hpp>
+#include <SFML/System/Vector2.hpp>
 #include <iostream>
 #include <vector>
-#include "map.hpp"
+#include "animation.hpp"
 
-class Player : public sf::Sprite {
+class Player : public sf::Drawable, public sf::Transformable {
  public:
-  Player();
-  explicit Player(const sf::Texture& t);
-  Player(const sf::Texture &t, const sf::IntRect &ir);
-  virtual ~Player();
-  // void draw(const sf::RenderWindow &window);
-  void update();
-  void Collision(Entity* entity);
-
-  const sf::Clock& getClock()     { return sprite_clock; }
-  const sf::Time&  getDelimiter() { return sprite_clock_delimiter; }
-  const std::vector<sf::IntRect>& getSpriteFrames() { return sprite_frames; }
-  const sf::IntRect& getSingleFrame(int frame) { return sprite_frames[frame]; }
-  void addSpriteFrame(const sf::IntRect &ir)   { sprite_frames.push_back(ir); }
-
-  sf::Sprite  sprite;
-  sf::Texture texture;
-
-  int rect_width  = 32;
-  int rect_height = 32;
-  int orgin_y     = 0;
+  explicit Player(sf::Time frameTime = sf::seconds(0.2f),
+                  bool paused = false, bool looped = true);
+  void update(sf::Time deltaTime);
+  void setAnimation(const Animation& animation);
+  void setFrameTime(sf::Time time);
+  void play();
+  void play(const Animation& animation);
+  void pause();
+  void stop();
+  void setLooped(bool looped);
+  void setColor(const sf::Color& color);
+  const Animation* getAnimation() const;
+  sf::FloatRect getLocalBounds() const;
+  sf::FloatRect getGlobalBounds() const;
+  bool isLooped() const;
+  bool isPlaying() const;
+  sf::Time getFrameTime() const;
+  void setFrame(std::size_t newFrame, bool resetTime = true);
 
  private:
-  sf::Clock sprite_clock;
-  sf::Time  sprite_clock_delimiter;
-  std::vector<sf::IntRect> sprite_frames;
-  int current_frame;
+  const Animation* animation_;
+  sf::Time frame_time_;
+  sf::Time current_time_;
+  std::size_t current_frame_;
+  bool is_paused_;
+  bool is_looped_;
+  const sf::Texture* texture_;
+  sf::Vertex vertices_[4];
+
+  virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
 };
 
 #endif  // PLAYER_HPP
--- Game/src/player.cpp
+++ Game/src/player.cpp
@@ -1,44 +1,144 @@
 // Copyright 2015, Aaron Ceross
 
-#include <SFML/Graphics.hpp>
 #include "../include/player.hpp"
-#include "../include/game_state_manager.hpp"
 
-Player::Player() :
-sf::Sprite(texture), sprite_clock_delimiter(sf::seconds(0.5f)),
-                                                            current_frame(0) {}
+Player::Player(sf::Time frameTime, bool paused, bool looped) :
+  animation_(NULL), frame_time_(frameTime), current_frame_(0),
+  is_paused_(paused), is_looped_(looped), texture_(NULL) {}
 
-Player::Player(const sf::Texture& t) :
-sf::Sprite(t), sprite_clock_delimiter(sf::seconds(0.5f)), current_frame(0) {}
-
-Player::Player(const sf::Texture &t, const sf::IntRect &ir) :
-sf::Sprite(t), sprite_clock_delimiter(sf::seconds(0.5f)), current_frame(0) {
-  sprite_frames.push_back(ir);
+void Player::setAnimation(const Animation& animation) {
+  animation_     = &animation;
+  texture_       = animation_->getSpriteSheet();
+  current_frame_ = 0;
+  setFrame(current_frame_);
 }
 
-void Player::update() {
-  // Check to see if elapsed time is greater than delimiter time
-  if (sprite_clock.getElapsedTime() > sprite_clock_delimiter) {
-    // Check to see if we have to reset the value of current_frame
-    if (sprite_frames.size() >= current_frame) {
-      ++current_frame;
-    } else {
-      // Reset the value
-      current_frame = 0;
+void Player::setFrameTime(sf::Time time) {
+  frame_time_ = time;
+}
+
+void Player::play() {
+  is_paused_ = false;
+}
+
+void Player::play(const Animation& animation_) {
+  if (getAnimation() != &animation_) {
+    setAnimation(animation_);
+  }
+  play();
+}
+
+void Player::pause() {
+  is_paused_ = true;
+}
+
+void Player::stop() {
+  is_paused_     = true;
+  current_frame_ = 0;
+  setFrame(current_frame_);
+}
+
+void Player::setLooped(bool looped) {
+  is_looped_ = looped;
+}
+
+void Player::setColor(const sf::Color& color) {
+  // Update the vertices' color
+  vertices_[0].color = color;
+  vertices_[1].color = color;
+  vertices_[2].color = color;
+  vertices_[3].color = color;
+}
+
+const Animation* Player::getAnimation() const {
+  return animation_;
+}
+
+sf::FloatRect Player::getLocalBounds() const {
+  sf::IntRect rect = animation_->getFrame(current_frame_);
+
+  float width  = static_cast<float>(std::abs(rect.width));
+  float height = static_cast<float>(std::abs(rect.height));
+
+  return sf::FloatRect(0.f, 0.f, width, height);
+}
+
+sf::FloatRect Player::getGlobalBounds() const {
+  return getTransform().transformRect(getLocalBounds());
+}
+
+bool Player::isLooped() const {
+  return is_looped_;
+}
+
+bool Player::isPlaying() const {
+  return !is_paused_;
+}
+
+sf::Time Player::getFrameTime() const {
+  return frame_time_;
+}
+
+void Player::setFrame(std::size_t newFrame, bool resetTime) {
+  if (animation_) {
+    // calculate new vertex positions and texture coordiantes
+    sf::IntRect rect = animation_->getFrame(newFrame);
+
+    vertices_[0].position = sf::Vector2f(0.f, 0.f);
+    vertices_[1].position = sf::Vector2f(0.f, static_cast<float>(rect.height));
+    vertices_[2].position = sf::Vector2f(static_cast<float>(rect.width),
+                                         static_cast<float>(rect.height));
+    vertices_[3].position = sf::Vector2f(static_cast<float>(rect.width), 0.f);
+
+    float left   = static_cast<float>(rect.left) + 0.0001f;
+    float right  = left + static_cast<float>(rect.width);
+    float top    = static_cast<float>(rect.top);
+    float bottom = top + static_cast<float>(rect.height);
+
+    vertices_[0].texCoords = sf::Vector2f(left, top);
+    vertices_[1].texCoords = sf::Vector2f(left, bottom);
+    vertices_[2].texCoords = sf::Vector2f(right, bottom);
+    vertices_[3].texCoords = sf::Vector2f(right, top);
+  }
+
+  if (resetTime) {
+    current_time_ = sf::Time::Zero;
+  }
+}
+
+void Player::update(sf::Time deltaTime) {
+  // if not paused and we have a valid animation
+  if (!is_paused_ && animation_) {
+    // add delta time
+    current_time_ += deltaTime;
+
+    // if current time is bigger then the frame time advance one frame
+    if (current_time_ >= frame_time_) {
+      // reset time, but keep the remainder
+      current_time_ = sf::microseconds(current_time_.asMicroseconds() %
+                                       frame_time_.asMicroseconds());
+
+      // get next Frame index
+      if (current_frame_ + 1 < animation_->getSize()) {
+        current_frame_++;
+      } else {
+        // animation has ended
+        // reset to start
+        current_frame_ = 0;
+        if (is_looped_) {
+          is_paused_ = true;
+        }
+      }
+      // set the current frame, not reseting the time
+      setFrame(current_frame_, false);
     }
-    // Set the clipping rect
-    setTextureRect(sprite_frames[current_frame]);
-
-    // Reset the Clock
-    sprite_clock.restart();
-  }
-
-  // Check for movement events
-  if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-    move(5.0f, 0.0f);
   }
 }
 
-// void Player::draw(const sf::RenderWindow &window) {}
-
-Player::~Player() {}
+void Player::draw(sf::RenderTarget& target, sf::RenderStates states) const {
+  if (animation_ && texture_) {
+    states.transform  *= getTransform();
+    states.texture     = texture_;
+    target.draw(vertices_, 4, sf::Quads, states);
+  }
+}
